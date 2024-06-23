b4e543545400c1f1b09527de31488a616d389957
NAME <wyscat@naver.com>
2024-06-23 10:29:28 +0900

    (0623)

--- sjw/src/App.js
+++ sjw/src/App.js
@@ -8,21 +8,19 @@ import Wish from "./components/Wish/Wish";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Router>
-        <div className="App">
-          <div className="content">
-            <Routes>
-              <Route exact path="/" element={<Intro />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/whatCanIDo" element={<WhatCanIDo />} />
-              <Route path="/wish" element={<Wish />} />
-            </Routes>
-          </div>
-          <Nav />
+    <Router basename={process.env.PUBLIC_URL}>
+      <div className="App">
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Intro />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/whatCanIDo" element={<WhatCanIDo />} />
+            <Route path="/wish" element={<Wish />} />
+          </Routes>
         </div>
-      </Router>
-    </BrowserRouter>
+        <Nav />
+      </div>
+    </Router>
   );
 }
 
