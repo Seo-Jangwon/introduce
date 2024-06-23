12c0552c632798d844f4495ac250af197131f667
NAME <wyscat@naver.com>
2024-06-23 16:40:48 +0900

    (0623) intro 수정

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -2,21 +2,25 @@ import React, { useState, useEffect } from "react";
 import styles from "./Intro.module.css";
 
 const Intro = () => {
-  const [visible, setVisible] = useState([true, false]);
+  const [visibleIndex, setVisibleIndex] = useState(0);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setVisible([true, true]);
-    }, 800);
+    const intervals = [
+      setTimeout(() => setVisibleIndex(1), 1000),
+      setTimeout(() => setVisibleIndex(2), 2000),
+      setTimeout(() => setVisibleIndex(3), 3000),
+    ];
 
-    return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 정리
+    // 클린업 함수에서 타이머 제거
+    return () => intervals.forEach(clearTimeout);
   }, []);
 
   return (
-    <div className={styles.centerContainer}>
+    <div className={styles.container}>
       <div className={styles.leftAlignedText}>
-        <h1 className={styles.visible}>안녕하세요</h1>
-        <h1 className={visible[1] ? styles.visible : styles.hidden}>
+        <h1>안녕하세요</h1>
+
+        <h1 className={visibleIndex >= 1 ? styles.visible : styles.hidden}>
           서장원입니다.
         </h1>
       </div>
