236b1cb98f625ba9c3346e9b879b114c2b1ae2c3
NAME <wyscat@naver.com>
2024-06-23 10:35:53 +0900

    (0623) browserrouter 수정

--- sjw/src/App.js
+++ sjw/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
 import Intro from "./components/Intro/Intro";
 import Nav from "./components/Nav/Nav";
 import About from "./components/About/About";
@@ -8,7 +8,7 @@ import Wish from "./components/Wish/Wish";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
         <div className="content">
           <Routes>
@@ -20,7 +20,7 @@ function App() {
         </div>
         <Nav />
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
